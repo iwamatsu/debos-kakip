91be7204e07e4747a9f958848fe9d34bb982be5a
Nobuhiro Iwamatsu <iwamatsu@nigauri.org>
2025-03-03 06:51:42 +0900

    scripts/extract_bibary.sh: Create ${OVERLAY_DIR} directory forcefully
    Signed-off-by: Nobuhiro Iwamatsu <iwamatsu@nigauri.org>

--- scripts/extract_bibary.sh
+++ scripts/extract_bibary.sh
@@ -6,6 +6,8 @@ IMG=kakip-es2_ubuntu_base_v5.img
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 OVERLAY_DIR=${SCRIPT_DIR}/../overlay/bootloader/opt/
 
+mkdir -p ${OVERLAY_DIR}
+
 dd if=${IMG} of=${OVERLAY_DIR}/header0.bin count=80 bs=1
 dd if=${IMG} of=${OVERLAY_DIR}/header1.bin skip=1 count=1
 dd if=${IMG} of=${OVERLAY_DIR}/bl2.bin skip=8 count=398
